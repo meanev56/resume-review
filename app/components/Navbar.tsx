12dac66365c8ae57befefa0fbc6bfa75bdd05208
meanev56 <emekanjoku86@gmail.com>
2025-08-05 10:02:14 +0100

    addition of navbar and app css

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <Link>
+      
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,13 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track YourApplications & Resume Rating</h1>
+        <h2>Review your submissions and check AI-powered feedback</h2>
+      </div>
+    </section>
+  </main>;
 }
